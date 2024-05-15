eb8b4e36b84d3c8f3f55e77e8b21c059576fedd7
akshit302005 <156989371+akshit302005@users.noreply.github.com>
2024-05-16 00:14:14 +0530

    updatation

---
+++ game1.0.py
@@ -0,0 +1,12 @@
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 5)
+
+def check_winner(board, player):
+    # Check rows
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+
+
